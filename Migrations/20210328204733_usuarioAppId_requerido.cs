05e9869e04b6381c942c1a32b4cb4bb4b86d5460
Diego Palacios <palaciosdiego759@gmail.com>
2021-03-28 14:50:50 -0600

    agregada: migracion usuarioAppId_requerido

---
+++ Migrations/20210328204733_usuarioAppId_requerido.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace supervisor_agente.Migrations
+{
+    public partial class usuarioAppId_requerido : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Actividades_AspNetUsers_usuarioAppId",
+                table: "Actividades");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "usuarioAppId",
+                table: "Actividades",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Actividades_AspNetUsers_usuarioAppId",
+                table: "Actividades",
+                column: "usuarioAppId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Actividades_AspNetUsers_usuarioAppId",
+                table: "Actividades");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "usuarioAppId",
+                table: "Actividades",
+                nullable: true,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Actividades_AspNetUsers_usuarioAppId",
+                table: "Actividades",
+                column: "usuarioAppId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
