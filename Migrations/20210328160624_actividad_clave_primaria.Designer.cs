8eb2bb0456caf0d5018f2a37a11da4ec7de88aef
Diego Palacios <palaciosdiego759@gmail.com>
2021-03-28 10:14:32 -0600

    agregada: migracion actividad_clave_primaria

--- Migrations/20210328160624_actividad_clave_primaria.Designer.cs
+++ Migrations/20210328160624_actividad_clave_primaria.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using supervisor_agente.Data;
 
 namespace supervisor_agente.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210328160624_actividad_clave_primaria")]
+    partial class actividad_clave_primaria
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20210328160624_actividad_clave_primaria.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace supervisor_agente.Migrations
+{
+    public partial class actividad_clave_primaria : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Actividades",
+                table: "Actividades");
+
+            migrationBuilder.DropColumn(
+                name: "correlativo",
+                table: "Actividades");
+
+            migrationBuilder.AddColumn<int>(
+                name: "id",
+                table: "Actividades",
+                nullable: false,
+                defaultValue: 0)
+                .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Actividades",
+                table: "Actividades",
+                column: "id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Actividades_asuntoId",
+                table: "Actividades",
+                column: "asuntoId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Actividades",
+                table: "Actividades");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Actividades_asuntoId",
+                table: "Actividades");
+
+            migrationBuilder.DropColumn(
+                name: "id",
+                table: "Actividades");
+
+            migrationBuilder.AddColumn<int>(
+                name: "correlativo",
+                table: "Actividades",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Actividades",
+                table: "Actividades",
+                columns: new[] { "asuntoId", "correlativo" });
+        }
+    }
+}
